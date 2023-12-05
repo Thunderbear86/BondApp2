6bb5dab2d9a900b12811e440d9d58161c60d8e74
Thunderbear86 <116293845+Thunderbear86@users.noreply.github.com>
2023-12-05 13:56:50 +0100

    init commit

--- index.php
+++ index.php
@@ -1,8 +1,35 @@
 <?php
 require "settings/init.php";
 
-$blogs = $db->sql("SELECT * FROM blogs");
+$dates = $db->sql("SELECT * FROM dates");
 
-foreach ($blogs as $blog){
-    echo $blog->blogName . "<br>";
+foreach ($dates as $date){
+    echo $date->dateName . "<br>";
 }
+?>
+
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Bond - Dating med mening</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
