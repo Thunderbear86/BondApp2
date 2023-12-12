66b99e38a0aa24faad884d0e9a883f55625796a1
Thunderbear86 <116293845+Thunderbear86@users.noreply.github.com>
2023-12-12 16:15:51 +0100

    profilnavn i oprettelse

--- po1.php
+++ po1.php
@@ -2,65 +2,47 @@
 <html lang="da">
 <head>
     <meta charset="utf-8">
-
     <title>Opret Profil - Profilnavn</title>
-
-    <meta name="robots" content="All">
-    <meta name="author" content="Udgiver">
-    <meta name="copyright" content="Information om copyright">
-
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-
 <body>
+
 <header>
 
 </header>
+
 <nav>
     <?php include "includes/nav.php";?>
 </nav>
 
 <main>
-
     <div class="container">
         <div class="row">
             <img src="img/LOGO.png" alt="logo" width="100" height="100">
         </div>
-    </div>
-
-    <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <p>Opret din profil og start dit møde</p>
-
-                <br>
-
-
-
-                <form action="opretlog.php" method="post">
+                <p class="mb-3">Opret din profil og start dit møde</p>
+                <form action="submit_username.php" method="post">
                     <div class="form-group">
-                        <label for="inputEmail"><h1>Profilnavn</h1></label>
-
-                        <input type="#" class="form-control" id="#" name="#" placeholder="Profilnavn">
-
-                    </form>
+                        <label for="inputProfilNavn"><h1>Profilnavn</h1></label>
+                        <input type="text" class="form-control mb-3" id="inputProfilNavn" name="username" placeholder="Profilnavn">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Frem</button>
+                </form>
             </div>
         </div>
     </div>
-                <button type="#" class="btn btn-primary">Tilbage</button>
-                <button type="#" class="btn btn-primary">Frem</button>
-
+</main>
 
 <footer>
     <?php include "includes/footer.php";?>
 </footer>
 
 <script src="js/main.js"></script>
-
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
-
---
+++ submit_username.php
@@ -0,0 +1,31 @@
+<?php
+require "settings/init.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+
+    // Simple validation
+    if (empty($username)) {
+        // Handle error - username is empty
+        exit("Username is required.");
+    }
+
+    // Insert into database
+    try {
+        $sql = "INSERT INTO moedts (username) VALUES (:username)";
+        $stmt = $db->prepare($sql);
+        $stmt->execute(['username' => $username]);
+
+        // Redirect or inform the user of successful creation
+        echo "Profile created successfully.";
+        // Redirect to another page if needed
+    } catch (PDOException $e) {
+        // Handle SQL errors (e.g., username already exists)
+        exit($e->getMessage());
+    }
+}
+
+// Redirect to form or show error if not POST request
+header("Location: path_to_your_form.php");
+exit();
+?>
